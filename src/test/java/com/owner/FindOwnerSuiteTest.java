c4fd2f3505766580582b1564c05e35a9beceb3c9
ParthaNath28 <partha.cemk@gmail.com>
2019-05-22 17:08:04 +0530

    Update FindOwnerSuiteTest.java

--- src/test/java/com/owner/FindOwnerSuiteTest.java
+++ src/test/java/com/owner/FindOwnerSuiteTest.java
@@ -61,7 +61,7 @@ public class FindOwnerSuiteTest {
 					DesiredCapabilities cap = DesiredCapabilities.chrome();
 					cap = DesiredCapabilities.chrome();
 					cap.setPlatform(org.openqa.selenium.Platform.WINDOWS);
-					driver = new RemoteWebDriver(new URL("http://192.168.41.56:5454/wd/hub"),cap);
+					driver = new RemoteWebDriver(new URL("http://127.0.0.1:5454/wd/hub"),cap);
 					
 					
 					
